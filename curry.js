b6f9dbae2e9d8bdc2b3ae048d9e2c9bc4e9357ee
Saket <saketp122@gmail.com>
2019-05-20 13:59:56 +0530

    info regarding esm modules

--- curry.js
+++ curry.js
@@ -1,5 +1,10 @@
+/*
+  Execute with experimental flag (node v12.2.0)
+  e.g. node v12.2.0: node --experimental-modules curry.js
+      node v10.15.3: node --experimental-modules curry.mjs (all js modules should have 'mjs' ext)
+*/
 import partial from './partial.js';
-//const partial = require('./partial.js');
+
 
 function curry(func) {
   return function curried(...args) {
